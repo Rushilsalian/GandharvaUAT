d22a5ebec274dea0a84638fe88d13229c7f90622
Rushil Salian <129928621+Rushilsalian@users.noreply.github.com>
2025-11-01 16:52:12 +0530

    pwaa

--- client/public/sw.js
+++ client/public/sw.js
@@ -38,28 +38,19 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match('/');
+      })
+    );
+    return;
+  }
+  
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-          return response;
-        });
-      })
-      .catch(() => {
-        if (event.request.destination === 'document') {
-          return caches.match('/');
-        }
+        return response || fetch(event.request);
       })
   );
 });
